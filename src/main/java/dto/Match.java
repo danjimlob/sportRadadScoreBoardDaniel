dd195017be55c3109dc107fdbb1eeffe545ac82e
danjimlob <danieep25@gmail.com>
2023-08-24 20:19:55 +0200

    Add model Match
    Added model basic Match

--- src/main/java/dto/Match.java
+++ src/main/java/dto/Match.java
@@ -1,31 +1,19 @@
 package dto;
 
-
 public class Match {
-	
-	private  String localTeam;
-	private int homeScore=0;
 
-	private  String visitorTeam;
-	private int visitorScorte=0;
+	private String localTeam;
+	private int homeScore = 0;
 
-	
-	private  int order;
-	
-	
+	private String visitorTeam;
+	private int visitorScorte = 0;
+
+	private int order;
+
 	public Match(String localTeam, String visitorTeam, int order) {
 		this.localTeam = localTeam;
 		this.visitorTeam = visitorTeam;
 		this.order = order;
-	} 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}
+
 }
