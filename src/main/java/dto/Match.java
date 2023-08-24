aba09cb0bdc388016f84d693f98c459dbc445dae
danjimlob <danieep25@gmail.com>
2023-08-24 20:18:33 +0200

    Initial commit

---
+++ src/main/java/dto/Match.java
@@ -0,0 +1,31 @@
+package dto;
+
+
+public class Match {
+	
+	private  String localTeam;
+	private int homeScore=0;
+
+	private  String visitorTeam;
+	private int visitorScorte=0;
+
+	
+	private  int order;
+	
+	
+	public Match(String localTeam, String visitorTeam, int order) {
+		this.localTeam = localTeam;
+		this.visitorTeam = visitorTeam;
+		this.order = order;
+	} 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
